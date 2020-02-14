606bec834e4c668067163b59350100e9774ceba7
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-14 08:38:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(number)
+  bool = true
+  if number < 2
+    bool =false
+  else
+    array = (2..number).to_a
+    i=2
+  
+    while i<number
+      if (number%i ==0)
+        bool = false
+        return bool
+        end
+      i+=1
+    end
+  end
+  bool
+end
