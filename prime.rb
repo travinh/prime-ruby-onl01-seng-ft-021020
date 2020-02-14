ff976525c7b9019ee9ef81adbd80b7e500af69eb
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-14 08:35:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 # Add  code here!
 def prime?(number)
-  array = (1..number).to_a
-  i=0
+  array = (2..number).to_a
+  i=2
   bool = true
   while i<number
     if (number %i ==0)
       return false
     i+=1
   end
+  bool
 end
